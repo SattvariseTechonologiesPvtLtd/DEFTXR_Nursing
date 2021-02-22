ffc23d03e659778ea5d8cb211ec205130f205b7f
Ishan Bhosekar <ishan.bhosekar9@gmail.com>
2021-02-22 17:57:25 +0530

    Ishan Bhosekar: Added Colliders
    Ishan Bhosekar: Added Colliders

---
+++ Assets/Nursing Procedures/Oxygen Administration/Scripts/Collision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    public GameObject OxygenCylinder;
+    public GameObject go2;
+    public GameObject go3;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag == "NGTube_Grabbable")
+        {
+
+            this.gameObject.SetActive(false);
+            go2.SetActive(false);
+
+            go3.SetActive(true);
+        }
+    }
+}
