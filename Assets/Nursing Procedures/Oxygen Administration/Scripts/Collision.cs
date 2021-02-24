f8911ea1338247becbacc69af91c52a295d2ae3b
Ishan Bhosekar <ishan.bhosekar9@gmail.com>
2021-02-24 12:22:51 +0530

    Revert "Revert "Ishan Bhosekar: Highlight Changes""
    This reverts commit ff00e674a12c305ceb1aa8a36eb50776212f2fa1.

--- Assets/Nursing Procedures/Oxygen Administration/Scripts/Collision.cs
+++ Assets/Nursing Procedures/Oxygen Administration/Scripts/Collision.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    public GameObject OxygenCylinder;
+    public GameObject Highlighted;
     public GameObject go2;
     public GameObject go3;
+  
 
     private void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.CompareTag == "NGTube_Grabbable")
+        if (col.gameObject.CompareTag("Highlighted"))
         {
 
             this.gameObject.SetActive(false);
             go2.SetActive(false);
+           
 
             go3.SetActive(true);
         }
