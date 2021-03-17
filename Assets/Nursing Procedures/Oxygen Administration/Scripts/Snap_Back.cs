70023cb0625a0b7f0b0ca2345c8e93830d8a1437
Ishan Bhosekar <ishan.bhosekar9@gmail.com>
2021-03-17 16:46:50 +0530

    Ishan Bhosekar:Changes Made
    Changes Made

---
+++ Assets/Nursing Procedures/Oxygen Administration/Scripts/Snap_Back.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snap_Back : MonoBehaviour
+{
+    public GameObject floorplane;
+    public GameObject equipment;
+        private Vector3 originalposition;
+    private Quaternion originalrotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        originalposition = equipment.gameObject.transform.position;
+        originalrotation = equipment.gameObject.transform.rotation;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name==floorplane.name)
+        {
+            equipment.gameObject.transform.position = originalposition;
+            equipment.gameObject.transform.rotation = originalrotation;
+        }
+    }
+}
