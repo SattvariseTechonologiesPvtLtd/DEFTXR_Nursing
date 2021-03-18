82f8c5a55437e9e29204ce63435e050917077930
CaptainNutzz <Vedantnatu99@gmail.com>
2021-03-18 10:49:31 +0530

    Revert "Revert "Revert "Merge branch 'draft_edit' of https://github.com/SattvariseTechonologiesPvtLtd/DEFTXR_Nursing into draft_edit"""
    This reverts commit e046d4f7d9a7ebdfd0af1fa52c500c544164f32b.

---
+++ Assets/Nursing Procedures/Oxygen Administration/Scripts/OA GAME MANAGER.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OAGAMEMANAGER : MonoBehaviour
+{
+    // list of the audioclips required
+    [SerializeField]
+    private List<AudioClip> intro_VO;
+
+    // Actions referencing Actions to be completed by user
+    [SerializeField]
+    private bool[] ActionsCompleted = { false };
+
+    // Guides referencing Guide Animation to be displayed on each step
+    [SerializeField]
+    private List<GameObject> Guides;
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    // Model of Patient lying on Bed
+    [SerializeField]
+    private GameObject patient;
+
+    private bool grabbedOnce1 = false;
+    private bool grabbedOnce2 = false;
+    private bool grabbedOnce3 = false;
+    private bool grabbedOnce4 = false;
+
+
+
+
+    [SerializeField]
+    private GameObject OxygenCylinder;
+    [SerializeField]
+    private GameObject TranstrachealCatheter;
+    [SerializeField]
+    private GameObject VenturiMask;
+    [SerializeField]
+    private GameObject NasalCanula;
+    [SerializeField]
+    private GameObject OxygenHood;
+    [SerializeField]
+    private GameObject NonRebreatherMask;
+    [SerializeField]
+    private GameObject FaceTent;
+    [SerializeField]
+    private GameObject WoldBottle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
