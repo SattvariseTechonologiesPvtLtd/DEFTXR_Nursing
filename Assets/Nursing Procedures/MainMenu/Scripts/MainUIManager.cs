20f3a68246068503345d566e9267f46580bf0e81
SattvariseTechonologiesPvtLtd <developer@sattvarise.com>
2021-03-30 14:48:42 +0530

    MainMenuAdded

---
+++ Assets/Nursing Procedures/MainMenu/Scripts/MainUIManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainUIManager : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onBloodTransfuton_Btn_Click()
+    {
+
+        SceneManager.LoadScene("");
+    }
+    public void onRTInsertion_Btn_Click()
+    {
+        SceneManager.LoadScene("");
+
+    }
+    public void onOxygenAdmin_Btn_Click()
+    {
+        SceneManager.LoadScene("");
+    }
+    public void onROS_Btn_Click()
+    {
+        SceneManager.LoadScene("");
+    }
+    public void onEmerygencyTrolly_Btn_Click()
+    {
+        SceneManager.LoadScene("");
+    }
+    
+}
