199c01944103fc77372b79654bca434c3aefa9d3
Sujit Mane <manesujit85@gmail.com>
2021-03-30 12:22:01 +0530

    Character added in Blood transfusion
    Character added in Blood transfusion
    Assets > Nursing procedures > Blood transfusion > BT New Animations

---
+++ Assets/Nursing Procedures/RT Insertion/Script/RT_Equipments.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RT_Equipments : MonoBehaviour
+{
+    public static RT_Equipments Instance;
+
+    public void Awake()
+    {
+        Instance = this;
+    }
+
+    //Original Equipments
+    public GameObject NASOGASTRIC_TUBE;
+
+    public GameObject MEASURE_TUBE;
+
+    public GameObject Fix_Insertion_TUBE;
+
+    public GameObject FIX_NASOGASTRIC_TUBE;
+
+    public GameObject WATER_SOLUBLE_LUBRICANT;
+
+    public GameObject GLASS_OF_WATER;
+
+    public GameObject TAPE;
+
+    public GameObject SmallTape;
+
+    public GameObject EMESIS_BASIN;
+
+}
