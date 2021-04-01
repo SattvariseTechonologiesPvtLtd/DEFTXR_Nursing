c6989fc1cfad35f36d279b006682a6eaa76562be
Your Name <Vedantnatu99@gmail.com>
2021-04-01 11:58:56 +0530

    Vedant ET

---
+++ Assets/Emergency Trolley/Scripts/myGrabbableObtManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class myGrabbableObtManager : MonoBehaviour
+{
+    public GameObject myGrabbaleObj;
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (String.Compare(other.gameObject.name, "touchInput") == 0)
+        {
+            Debug.Log("workinggggg");
+
+            myGrabbaleObj.SetActive(true);
+            myGrabbaleObj.GetComponent<BoxCollider>().enabled = true;
+            myGrabbaleObj.GetComponent<Rigidbody>().useGravity = true;
+            myGrabbaleObj.GetComponent<Rigidbody>().isKinematic = false;
+
+            //BT_GameManager.Instance.touch_input.SetActive(false);
+
+            this.gameObject.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("not working");
+        }
+    }
+
+ 
+}
--- Assets/Nursing Procedures/Removal of Sutures/HandsOnVR/Scripts/Extensions/ArrayExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace HandsOnVR
-{
-	public static class ArrayExtensions
-	{
-		public static bool IsNullOrEmpty(this Array array) => (array == null || array.Length == 0);
-	}
-}
